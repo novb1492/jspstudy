8abaeb02b7d1b57f6beb3b9464a2d0105291f54c
novb1492 <novb1492@naver.com>
2021-04-27 22:38:09 +0900

    mysql insert/delete

--- src/main/java/studyjdbc/delete.java
+++ src/main/java/studyjdbc/delete.java
@@ -1,11 +1,12 @@
 package studyjdbc;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
 
-public class studyjdbc {
+public class delete {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		String id="root";
 		String pwd="6937544";
 		String url="jdbc:mysql://localhost:3306/study_db";
@@ -16,7 +17,7 @@ public class studyjdbc {
 		 conn=DriverManager.getConnection(url, id, pwd);///진짜 php 보다훨씬 짜증난다 ㅋㅋ
 		System.out.println(conn+"접속완료");
 		stmt=conn.createStatement();
-		String sql="INSERT INTO member(id,pwd,name,position)VAlUES('ki52','1234','jun','mid')";
+		String sql="DELETE FROM member WHERE id='kim'";
 		int a=stmt.executeUpdate(sql);
 		if(a==1)
 		{
@@ -42,4 +43,6 @@ public class studyjdbc {
 		}
 	}
 
-}
+	}
+
+
